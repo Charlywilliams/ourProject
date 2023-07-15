a6ef0c31145738de98ac47da9488470e1dc551c5
Jo-JoH-T <108591008+Jo-JoH-T@users.noreply.github.com>
2023-07-15 11:12:13 +0100

    Added Bootstrap

--- CFG_JavaScript.js
+++ CFG_JavaScript.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {myFunction()};
 
 // Get the navbar
-var navbar = document.getElementById("navbar");
+var navbar = document.getElementById("NavBar");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
